5137811c6ff473884bb347541311839bfcbed4d2
Levi Morrison <levi.morrison@datadoghq.com>
2022-02-07 08:01:46 -0700

    Modify PHP install steps for v0.70.0

--- utils/build/docker/php/common/install_ddtrace.sh
+++ utils/build/docker/php/common/install_ddtrace.sh
@@ -3,44 +3,26 @@
 set -eu
 
 echo "Loading install script"
-curl -Lf -o /tmp/dd-library-php-setup.php \
-  https://raw.githubusercontent.com/DataDog/dd-appsec-php/installer/dd-library-php-setup.php
+curl -Lf -o /tmp/datadog-setup.php \
+  https://github.com/DataDog/dd-trace-php/releases/download/0.70.0/datadog-setup.php
 
 cd /binaries
 
-BINARIES_APPSEC_N=$(find . -name 'dd-appsec-php-*.tar.gz' | wc -l)
-BINARIES_TRACER_N=$(find . -name 'datadog-php-tracer*.tar.gz' | wc -l)
-INSTALLER_ARGS=()
-if [[ $BINARIES_APPSEC_N -eq 1 ]]; then
-  INSTALLER_ARGS+=(--appsec-file /binaries/dd-appsec-php-*.tar.gz)
-elif [[ $BINARIES_APPSEC_N -gt 1 ]]; then
-  echo "Too many appsec packages in /binaries" >&2
-  exit 1
-else
-  INSTALLER_ARGS+=(--appsec-version $APPSEC_VERSION)
-fi
-
-if [[ $BINARIES_TRACER_N -eq 1 ]]; then
-  INSTALLER_ARGS+=(--tracer-file /binaries/datadog-php-tracer*.tar.gz)
-elif [[ $BINARIES_TRACER_N -gt 1 ]]; then
-  echo "Too many appsec packages in /binaries" >&2
-  exit 1
-else
-  INSTALLER_ARGS+=(--tracer-version $TRACER_VERSION)
-fi
-
-echo "Install args are ${INSTALLER_ARGS[@]}"
-
 export DD_APPSEC_ENABLED=0
-PHP_INI_SCAN_DIR=/etc/php/ php /tmp/dd-library-php-setup.php \
-  "${INSTALLER_ARGS[@]}"\
+PHP_INI_SCAN_DIR=/etc/php/ php /tmp/datadog-setup.php \
+  --enable-appsec \
+  --enable-profiling \
   --php-bin all
 
-php -d extension=ddtrace.so -d extension=ddappsec.so -r 'echo phpversion("ddtrace");' > \
+php -r 'echo phpversion("ddtrace");' > \
   ./SYSTEM_TESTS_LIBRARY_VERSION
 
-php -d extension=ddtrace.so -d extension=ddappsec.so -r 'echo phpversion("ddappsec");' > \
+php -r 'echo phpversion("ddappsec");' > \
   ./SYSTEM_TESTS_PHP_APPSEC_VERSION
+
+# What are these files for? todo: use profiling version
+php -r 'echo phpversion("datadog-profiling");' > \
+  ./SYSTEM_TESTS_PHP_PROFILING_VERSION
 
 touch SYSTEM_TESTS_LIBDDWAF_VERSION
 
@@ -48,4 +30,5 @@ find /opt -name ddappsec-helper -exec ln -s '{}' /usr/local/bin/ \;
 mkdir -p /etc/dd-appsec
 find /opt -name recommended.json -exec ln -s '{}' /etc/dd-appsec/ \;
 
-rm -rf /tmp/{dd-library-php-setup.php,dd-library,dd-appsec}
+# TODO: verify that the latest installer is cleaning all these up
+#rm -rf /tmp/{dd-library-php-setup.php,dd-library,dd-appsec}
